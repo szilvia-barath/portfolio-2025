d670e182b6955c244f5593b8c5c50e6608a64dc8
szilvia-barath <157715907+szilvia-barath@users.noreply.github.com>
2025-12-27 17:38:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "/App.jsx";
+import "/index.css";
 
 const root = document.getElementById("root");
 
